ea64267e5f942457853bd3b4a8abe80379ed2c16
brady-kimball <kimball.brady@gmail.com>
2017-05-15 09:46:24 -0700

    Add bench seeds

--- benchBnB/db/seeds.rb
+++ benchBnB/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Bench.destroy_all
+
+b1 = Bench.create(description: "Lafayette Park Bench",
+                  lat: 37.7915357,
+                  lng: -122.4307172
+                 )
+b2 = Bench.create(description: "Alta Plaza Bench",
+                  lat: 37.7907092,
+                  lng: -122.440314
+                 )
+b3 = Bench.create(description: "Nice bench",
+                  lat: 37.8008612,
+                  lng: -122.411985
+                 )
