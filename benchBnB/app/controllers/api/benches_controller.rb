b2c265f891cdab406380444a774512781bfe2211
brady-kimball <kimball.brady@gmail.com>
2017-05-15 09:58:37 -0700

    Add api/bench#create route and controller

--- benchBnB/app/controllers/api/benches_controller.rb
+++ benchBnB/app/controllers/api/benches_controller.rb
@@ -5,8 +5,23 @@ class Api::BenchesController < ApplicationController
     render :index
   end
 
+  def create
+    @bench = Bench.new(bench_params)
+    if @bench.save
+      render :show
+    else
+      render json: @bench.errors.full_messages, status: 422
+    end
+  end
+
   def show
     @bench = Bench.find(params[:id])
     render :show
+  end
+
+  private
+
+  def bench_params
+    params.require(:bench).permit(:description, :lat, :lng)
   end
 end
